d69366c498426a29aba41aaf8a693da693435890
Escape <240901017@qq.com>
2021-01-02 22:40:56 +0800

    feat: add static files

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask.helpers import url_for
 
 from data import movies, name
 
@@ -9,8 +8,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', name=name, movies=movies)
-
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User page: {name}.'
